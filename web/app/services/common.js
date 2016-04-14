6fb3c623cc53e2e1803cd013d3e0f910473ad53c
ritesh-gupta <riteshkumar.gupta@talentica.com>
2016-04-14 16:33:57 +0530

    Updated design

---
+++ web/app/services/common.js
@@ -0,0 +1,17 @@
+jQuery.noConflict();
+jQuery(window).load(function() {
+	jQuery(document).on('focus', '.form-control', function() {
+		jQuery(this).parent().addClass('focused');
+	});
+	jQuery(document).on('focusout', '.form-control', function() {
+		jQuery(this).parent().removeClass('focused');
+	});
+	jQuery('input').each(function(){
+		if(jQuery(this).text() != ""){
+		jQuery(this).parent().addClass('focused');
+		}
+	});
+	jQuery.each(jQuery('input[type=text]'),function(){
+	alert(jQuery(this).val());
+	});
+});
